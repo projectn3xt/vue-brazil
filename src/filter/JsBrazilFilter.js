764be3804500db7697dfb677e43899f32d12716d
Marcelo Gomes <marcelo@n3xt.com.br>
2020-10-01 15:38:13 -0300

    Update JsBrazilFilter.js
    remove console.log

--- src/filter/JsBrazilFilter.js
+++ src/filter/JsBrazilFilter.js
@@ -2,6 +2,6 @@ import { maskBr } from 'js-brasil';
 
 // USAGE: {{ message | filterA('arg1', arg2) }}
 export default (value, tipo, args) => {
-  console.log(value, tipo)
+  // console.log(value, tipo)
   return maskBr[tipo](value);
 }
